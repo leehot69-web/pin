d5d9ea3a247155ab2e146934fd195ffa076a1543
Tu Nombrejuan <tu_email@ejemplo.com>
2026-02-11 16:02:32 -0400

    SHIELD: Force inject env vars into build config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://dsfeibnvihpvobtlkewz.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzZmVpYm52aWhwdm9idGxrZXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzOTY2MzEsImV4cCI6MjA4NDk3MjYzMX0.iZYLiyoCb85NAYxGtw_eDqUAYRPUFIDIGK8hFLHL160',
+  }
 };
 
 export default nextConfig;
